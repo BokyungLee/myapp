9bba0b85c9f34a3924036e0c2810d17db746b4f8
bokidoki <lbk3441@naver.com>
2024-07-29 15:13:13 +0900

    ok3

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Have a good day123");
+        System.out.println("어렵당");
     }
 }
