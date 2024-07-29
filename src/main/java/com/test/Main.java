489ce77e6c145636d57b7796dad147553c278b5b
bokidoki <lbk3441@naver.com>
2024-07-29 15:13:55 +0900

    ok4

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -3,7 +3,8 @@ package com.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Have a good day123");
+        System.out.println("Have a good day12345");
         System.out.println("어렵당");
+        System.out.println("짱어렵당");
     }
 }
